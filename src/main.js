dc78ef89fe4746044933b4af57c5eddf0923d897
Than Quoc Tuan <tuanB2207574@student.ctu.edu.vn>
2024-10-22 18:46:44 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-
+import '@fortawesome/fontawesome-free'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import mitt from 'mitt'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
 import router from './router'
-
 
 const emitter = mitt()
 const app = createApp(App)
