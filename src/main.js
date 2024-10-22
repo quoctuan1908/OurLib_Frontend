701e05914b6ea1c4796b3db5cf657446f8ae8c4d
Than Quoc Tuan <tuanB2207574@student.ctu.edu.vn>
2024-10-22 18:24:36 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // import '@fortawesome/fontawesome-free'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import mitt from 'mitt'
 import { createApp } from 'vue'
 import App from './App.vue'
